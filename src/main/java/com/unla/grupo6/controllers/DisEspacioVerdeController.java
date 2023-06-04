53cc10ecc9545e92be2a9d3be811970e07792f0c
Bruno Massacane <brunomassacane@gmail.com>
2023-06-04 17:48:41 -0300

    trabajo controller espacio verde aunque debo solucionar cosas

--- src/main/java/com/unla/grupo6/controllers/DisEspacioVerdeController.java
+++ src/main/java/com/unla/grupo6/controllers/DisEspacioVerdeController.java
@@ -1,5 +1,29 @@
 package com.unla.grupo6.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.unla.grupo6.helpers.ViewRouterHelper;
+
+@Controller
+@RequestMapping("/disEspacioVerde")
 public class DisEspacioVerdeController {
+	
+//	@Autowired
+//	@Qualifier("espacioVerdeService")
+	//private IEspacioVerdeService espacioVerdeService; //buscar como hacer que neceisot para que funcione
+	
+	//private ModelMapper modelMapper = new ModelMapper();
+	
+//	@GetMapping("")
+//	public ModelAndView index() {
+//		ModelAndView mAV = new ModelAndView(ViewRouteHelper.ESPACIOVERDE_INDEX); //por que no me lo reconoce y me pide un archivo del tfi gustavo?
+//		mAV.addObject("espaciosVerdes", espacioVerdeService.getAll());
+//		return mAV;
+//	}
 
 }
--- src/main/java/com/unla/grupo6/helpers/ViewRouterHelper.java
+++ src/main/java/com/unla/grupo6/helpers/ViewRouterHelper.java
@@ -1,5 +1,8 @@
 package com.unla.grupo6.helpers;
 
 public class ViewRouterHelper {
-
+	/**** Views ****/
+	
+	//ESPACIO VERDE
+	public final static String INDEX =  "disEspacioVerde/index";
 }
