693e55045d188ff32dda662273956620716454f9
Dante Lugo <dantelugo1505@gmail.com>
2023-06-08 15:22:15 -0300

    comente la clase IDispositivoREpopsitory

--- src/main/java/com/unla/grupo6/repositories/IDispositivoRepository.java
+++ src/main/java/com/unla/grupo6/repositories/IDispositivoRepository.java
@@ -1,29 +1,29 @@
-package com.unla.grupo6.repositories;
-
-import java.io.Serializable;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import com.unla.grupo6.entities.Dispositivo;
-
-public interface IDispositivoRepository extends JpaRepository <Dispositivo, Serializable>{
-		
-		public abstract Dispositivo findById(int id);
-		
-		public abstract Dispositivo findByNombre(String nombre);
-		
-		public abstract Dispositivo findByDescripcion(String descripcion);
-		
-		public abstract Dispositivo findByFuncionamiento(boolean enFuncionamiento);
-		
-		//revisar que tipo de query hay que hacer para el tp 
-		//debo traer una lista?
-		@Query("SELECT d FROM Dispositivo d JOIN FETCH d.disEspacioVerde de WHERE de.nombre = (:nombre)")
-		public abstract Dispositivo findByEspacioVerdeNombre(String nombre);
-		
-		
-		
-}
+//package com.unla.grupo6.repositories;
+//
+//import java.io.Serializable;
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import com.unla.grupo6.entities.Dispositivo;
+//
+//public interface IDispositivoRepository extends JpaRepository <Dispositivo, Serializable>{
+//		
+//		public abstract Dispositivo findById(int id);
+//		
+//		public abstract Dispositivo findByNombre(String nombre);
+//		
+//		public abstract Dispositivo findByDescripcion(String descripcion);
+//		
+//		public abstract Dispositivo findByFuncionamiento(boolean enFuncionamiento);
+//		
+//		//revisar que tipo de query hay que hacer para el tp 
+//		//debo traer una lista?
+//		@Query("SELECT d FROM Dispositivo d JOIN FETCH d.disEspacioVerde de WHERE de.nombre = (:nombre)")
+//		public abstract Dispositivo findByEspacioVerdeNombre(String nombre);
+//		
+//		
+//		
+//}
