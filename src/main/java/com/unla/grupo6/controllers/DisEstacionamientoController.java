850f085ceebec771cda80992e423f23d04f4c8e6
Nicolas Naceli <naceli.cuenta.profesional@gmail.com>
2023-06-08 16:28:58 -0300

    error en nombre de archivo modificar solucionado

--- src/main/java/com/unla/grupo6/controllers/DisEstacionamientoController.java
+++ src/main/java/com/unla/grupo6/controllers/DisEstacionamientoController.java
@@ -2,34 +2,43 @@ package com.unla.grupo6.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/estacionamiento")
 public class DisEstacionamientoController {
 	
-	@GetMapping("index")
+	@GetMapping("/index")
 	public String index() {
 		return "DisEstacionamiento/index";
 	}
 	
-	@GetMapping("estacionamientoDisponibles")
+	@GetMapping("/estacionamientoDisponibles")
 	public String estacionamientoDisponibles() {
 		return "DisEstacionamiento/estacionamientosGeneral";
 	}
 	
-	@GetMapping("agregar")
+	@GetMapping("/agregar")
 	public String agregar() {
 		return "DisEstacionamiento/agregarDispositivo";
 	}
 	
-	@GetMapping("modificar")
+	@GetMapping("/agregar/{sector}")
+	public ModelAndView agregarDisEstacionamiento(@PathVariable("sector") String sector) {
+		ModelAndView mV = new ModelAndView("DisEstacionamiento/agregarDispositivo");
+		mV.addObject("sector", sector);
+		return mV;
+	}
+	
+	@GetMapping("/modificar")
 	public String modificar() {
 		return "DisEstacionamiento/modificarDispositivo";
 	}
 	
-	@GetMapping("eliminar")
+	@GetMapping("/eliminar")
 	public String eliminar() {
 		return "DisEstacionamiento/eliminarDispositivo";
 	}
