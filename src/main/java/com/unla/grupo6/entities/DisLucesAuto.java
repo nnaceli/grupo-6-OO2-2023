fcf176d6cb2a84d6ad69b80a20672de16c6e79db
Dante Lugo <dantelugo1505@gmail.com>
2023-06-07 22:36:11 -0300

    agregue entities-controllers-template de luces automaticas

---
+++ src/main/java/com/unla/grupo6/entities/DisLucesAuto.java
@@ -0,0 +1,56 @@
+package com.unla.grupo6.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class DisLucesAuto extends Dispositivo {
+	private boolean estado;
+	private int nroAula;
+
+	public DisLucesAuto(int id, String nombre, String descripcion, boolean enFuncionamiento, boolean estado,
+			int nroAula) {
+		super(id, nombre, descripcion, enFuncionamiento);
+		this.estado = estado;
+		this.nroAula = nroAula;
+	}
+
+	public boolean verificarLuz() {
+		boolean luz = false;
+		if (this.estado == true) {
+			// esto esta mal tengo que pensarlo aiuda
+		}
+		return luz;
+	}
+
+	// ver con brandon
+	public boolean cambiarEstado() {
+		boolean estado = false;
+		if (this.estado == true) {
+			estado = true;
+		}
+		this.setEstado(estado);
+		return true;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public int getNroAula() {
+		return nroAula;
+	}
+
+	public void setNroAula(int nroAula) {
+		this.nroAula = nroAula;
+	}
+
+}
