e6c25c78b61cf4053db45e269a7a407e94880026
Nicolas Naceli <naceli.cuenta.profesional@gmail.com>
2023-06-08 16:42:55 -0300

    Primer paso dingo de un GET

--- src/main/java/com/unla/grupo6/helpers/ViewRouterHelper.java
+++ src/main/java/com/unla/grupo6/helpers/ViewRouterHelper.java
@@ -3,9 +3,14 @@ package com.unla.grupo6.helpers;
 public class ViewRouterHelper {
 	/**** Views ****/
 	
-	//ESPACIO VERDE ver bien como hacerlas
+	//ESPACIO VERDE ver bien como hacerlas 
 	public final static String ESPACIOVERDE_INDEX =  "espacioVerde/index";
 	
 	//ESTACIONAMIENTO
-	//public final
+	
+	
+	//BANIOS
+	
+	
+	//LUCES
 }
