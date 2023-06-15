1c28e077a19c38bc29430c3002d86d99af203a03
Nicolas Naceli <naceli.cuenta.profesional@gmail.com>
2023-06-15 15:30:46 -0300

    se agregaron dos metodos a estacionamientoRepository

--- src/main/java/com/unla/grupo6/repositories/IEstacionamientoRepository.java
+++ src/main/java/com/unla/grupo6/repositories/IEstacionamientoRepository.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.unla.grupo6.entities.DisEstacionamiento;
@@ -11,8 +12,10 @@ import com.unla.grupo6.entities.DisEstacionamiento;
 @Repository("estacionamientoRepository")
 public interface IEstacionamientoRepository extends JpaRepository<DisEstacionamiento, Serializable> {
 	
-	//public abstract DisEstacionamiento findById(int id);
-	
+	@Query("SELECT d FROM DisEstacionamiento d where d.sector = (:sector)")
 	public abstract List<DisEstacionamiento> findBySector(String sector);
+	
+	@Query("SELECT d FROM DisEstacionamiento d where d.sector = (:sector) AND d.ocupado = false")
+	public abstract List<DisEstacionamiento> findBySectorLibres(String sector);
 	
 }
