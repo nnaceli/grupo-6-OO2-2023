c225e628f3c299359351cce86ee64ec443f85d8e
Agustina Trarifa <agusstarifa@gmail.com>
2023-06-17 21:34:54 -0300

    arreglue el yml

--- src/main/java/com/unla/grupo6/entities/DisBaño.java
+++ src/main/java/com/unla/grupo6/entities/DisBaño.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.NotNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
