41c2bf1efb02a396866ba20c9a7aa229313c14e4
Nicolas Naceli <naceli.cuenta.profesional@gmail.com>
2023-06-21 00:54:05 -0300

    proceso base scheduler

--- src/main/java/com/unla/grupo6/Grupo6Application.java
+++ src/main/java/com/unla/grupo6/Grupo6Application.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.unla.grupo6.repositories.IEspacioVerdeRepository;
 import com.unla.grupo6.repositories.IEstacionamientoRepository;
@@ -18,6 +20,7 @@ import com.unla.grupo6.implementation.EstacionamientoService;
 import com.unla.grupo6.implementation.EventoService;
 
 @SpringBootApplication
+@EnableScheduling
 public class Grupo6Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -39,5 +42,10 @@ public class Grupo6Application implements CommandLineRunner {
 		for (DisEstacionamiento estacionamiento : Estacionamientos)
 			System.out.println(estacionamiento.toString());
 
+	}
+	
+	@Scheduled(cron = "*/60 * * * * *")
+	public void generarEventoDispositivoEstacionamiento() {
+		System.out.println("Nuevo Evento generado");
 	}
 }
