d5d7e5d832621d92e0ec577e5df7bdf086e66b75
Agustina Trarifa <agusstarifa@gmail.com>
2023-06-21 00:03:26 -0300

    commit
    agrego home

--- src/main/java/com/unla/grupo6/TestBCryptPasswordEncoder.java
+++ src/main/java/com/unla/grupo6/TestBCryptPasswordEncoder.java
@@ -6,7 +6,7 @@ public class TestBCryptPasswordEncoder {
 	public static void main(String[] args) {
 		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
 
-		System.out.println(pe.encode("pepito"));
+		System.out.println(pe.encode("user"));
 
 
 	}
