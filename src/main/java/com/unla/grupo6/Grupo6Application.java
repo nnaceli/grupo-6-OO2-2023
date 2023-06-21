559929732c2617c54c3b63fe564666a674979718
Bruno <brunomassacane@gmail.com>
2023-06-21 14:07:28 -0300

    paso importante en los permisos

--- src/main/java/com/unla/grupo6/Grupo6Application.java
+++ src/main/java/com/unla/grupo6/Grupo6Application.java
@@ -48,8 +48,8 @@ public class Grupo6Application implements CommandLineRunner {
 //			System.out.println(evento.toString());
 	}
 	
-	@Scheduled(cron = "*/3 * * * * *")
-	public void generarEventoDispositivoEstacionamiento() {
-		servicioEstacionamiento.actualizarDisponibilidadEstacionamientos();
-	}
+//	@Scheduled(cron = "*/3 * * * * *")
+//	public void generarEventoDispositivoEstacionamiento() {
+//		servicioEstacionamiento.actualizarDisponibilidadEstacionamientos();
+//	}
 }
