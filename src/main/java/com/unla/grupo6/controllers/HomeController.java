db95e00422bf578341d32cff28635865eeafac70
Agustina Trarifa <agusstarifa@gmail.com>
2023-06-21 21:21:11 -0300

    commit
    probando

--- src/main/java/com/unla/grupo6/controllers/HomeController.java
+++ src/main/java/com/unla/grupo6/controllers/HomeController.java
@@ -2,19 +2,18 @@ package com.unla.grupo6.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.unla.grupo6.helpers.ViewRouterHelper;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
-	@GetMapping("/index")
-	public ModelAndView index(@ModelAttribute("loginSuccess") Boolean loginSuccess) {
-		ModelAndView modelAndView = new ModelAndView(ViewRouterHelper.HOME_INDEX);
-
-		return modelAndView;
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mAV = new ModelAndView(ViewRouterHelper.HOME_INDEX);
+		return mAV;
 	}
 }
